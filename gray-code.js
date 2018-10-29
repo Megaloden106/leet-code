4535549a7c1858f8f80197511b0b03fbd7c85753
EddieLo <eddielo106@gmail.com>
2018-10-29 16:27:00 -0700

    init gray-code-89

---
+++ gray-code.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @return {number[]}
+ */
+var grayCode = function(n) {
+    let results;
+    const findGrayCode = (array = [0]) => {
+        if (array.length === 1 << n) {
+            return results = array;
+        }
+        const num = array[array.length - 1];
+        const options = [];
+        for (let i = 0; i < n; i += 1) {
+            options.push(num ^ (1 << i));
+        };
+        options.forEach(elem => {
+            if (!array.includes(elem)) {
+                const temp = array.slice();
+                temp.push(elem);
+                findGrayCode(temp);
+            }
+        });
+    };
+    findGrayCode();
+};
+
+console.log(grayCode(3));
