65924ce35c64373b5ae8905e8ae3a8c828aa87b3
EddieLo <eddielo106@gmail.com>
2018-10-30 12:10:05 -0700

    remove example from arranging coins and init large group pos

--- arranging-coins.js
+++ arranging-coins.js
@@ -4,28 +4,6 @@
 
 // n is a non-negative integer and fits within the range of a 32-bit signed integer.
 
-// Example 1:
-
-// n = 5
-
-// The coins can form the following rows:
-// ¤
-// ¤ ¤
-// ¤ ¤
-
-// Because the 3rd row is incomplete, we return 2.
-// Example 2:
-
-// n = 8
-
-// The coins can form the following rows:
-// ¤
-// ¤ ¤
-// ¤ ¤ ¤
-// ¤ ¤
-
-// Because the 4th row is incomplete, we return 3.
-
 /**
  * @param {number} n
  * @return {number}
