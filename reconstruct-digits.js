3425e91b419b8bb4c744d2689eb24f8514d603e8
EddieLo <eddielo106@gmail.com>
2018-11-02 13:44:31 -0700

    init reconstruct digits

---
+++ reconstruct-digits.js
@@ -0,0 +1,28 @@
+// Given a non-empty string containing an out-of-order English representation of digits 0-9, output the digits in ascending order.
+
+// Note:
+// Input contains only lowercase English letters.
+// Input is guaranteed to be valid and can be transformed to its original digits. That means invalid inputs such as "abc" or "zerone" are not permitted.
+// Input length is less than 50,000.
+
+const wordToDigit = {
+  zero: 0,
+  one: 1,
+  two: 2,
+  three: 3,
+  four: 4,
+  five: 5,
+  six: 6,
+  seven: 7,
+  eight: 8,
+  nine: 9,
+}
+
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var originalDigits = function(s) {
+  
+};
