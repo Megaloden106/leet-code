9ebd1ab1265a64379a185542ae7c9f9cfa2c59af
EddieLo <eddielo106@gmail.com>
2018-11-02 13:59:13 -0700

    incomplete solution for reconstruct digits

--- reconstruct-digits.js
+++ reconstruct-digits.js
@@ -18,11 +18,37 @@ const wordToDigit = {
   nine: 9,
 }
 
-
 /**
  * @param {string} s
  * @return {string}
  */
 var originalDigits = function(s) {
-  
+  let result = '';
+  let storage = {};
+  for (let i = 0; i < s.length; i += 1) {
+    storage[s[i]] = storage[s[i]] || 0;
+    storage[s[i]] += 1;
+  }
+  for (let key in wordToDigit) {
+    let contains = true;
+    while (contains) {
+      for (let i = 0; i < key.length; i += 1) {
+        if (!storage[key[i]]) {
+          contains = false;
+        }
+      }
+      if (contains) {
+        result += wordToDigit[key];
+        for (let i = 0; i < key.length; i+= 1) {
+          storage[key[i]] -= 1;
+        }
+      }
+    }
+  }
+  return result;
 };
+
+console.log(originalDigits('owoztneoer'));
+console.log(originalDigits('fviefuro'));
+console.log(originalDigits('twotwotwo'));
+console.log(originalDigits('zeroonetwothreefourfivesixseveneightnine'));
