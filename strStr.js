47f110347813c63aaadee27b6f78c38a33e4be1e
EddieLo <eddielo106@gmail.com>
2018-11-09 16:35:24 -0800

    complete strStr()

--- strStr.js
+++ strStr.js
@@ -5,6 +5,35 @@
  * @param {string} needle
  * @return {number}
  */
-var strStr = function(haystack, needle) {
-  
+const strStr = function(haystack, needle) {
+  if (haystack === needle) { return 0; }
+  for (let i = 0; i < haystack.length - needle.length + 1; i += 1) {
+    if (haystack.slice(i, i + needle.length) === needle) { return i; }
+  }
+  return -1;
 };
+
+// let haystack = "hello"
+// let needle = "ll"
+
+// console.log(strStr(haystack, needle));
+
+// haystack = "aaaaa"
+// needle = "bba"
+
+// console.log(strStr(haystack, needle));
+
+// haystack = ""
+// needle = ""
+
+// console.log(strStr(haystack, needle));
+
+// haystack = "mississippi"
+// needle = "issip"
+
+// console.log(strStr(haystack, needle));
+
+haystack = "mississippi"
+needle = "pi"
+
+console.log(strStr(haystack, needle));
