711342204191152a55052ea9e36e034ae8957ef6
EddieLo <eddielo106@gmail.com>
2018-11-13 16:43:35 -0800

    complete gray code

--- gray-code.js
+++ gray-code.js
@@ -1,9 +1,20 @@
 /**
- * 89 (monday 11/4)
  * @param {number} n
  * @return {number[]}
  */
 var grayCode = function(n) {
+  if (n === 0) { return [0]; }
+  const code = grayCode(n - 1);
+  if (n === 1) {
+    code.push(1);
+  } else {
+    const l = code.length - 1;
+    const mask = 1 << (n - 1);
+    for (let i = l; i >= 0; i -= 1) {
+      code.push(code[i] | mask)
+    }
+  }
+  return code;
 };
 
 console.log(grayCode(3));
