f3994a82a3343303da4cfdc4cc46b8c5e42f0558
EddieLo <eddielo106@gmail.com>
2018-11-15 15:33:28 -0800

    complete valid palindrome ii

--- valid-palindrome-ii.js
+++ valid-palindrome-ii.js
@@ -4,11 +4,21 @@
  * @param {string} s
  * @return {boolean}
  */
-const validPalindrome = (s) => {
-  let skips = 0;
-  for (let i = 0; i < s.length; i += 1) {
-
+const validPalindrome = (s, skips = 0) => {
+  let m = s.length / 2;
+  for (let i = 0, j = s.length - 1; i < m; i += 1, j -= 1) {
+    if (s[i] != s[j]) {
+      skips += 1;
+      if (skips > 1) { return false; }
+      return validPalindrome(s.slice(0, i) + s.slice(i + 1), skips + 1)
+        || validPalindrome(s.slice(0, j) + s.slice(j + 1), skips + 1);
+    }
   }
   return true;
 };
 
+// console.log(validPalindrome('aba'));
+// console.log(validPalindrome('abc'));
+// console.log(validPalindrome('abba'));
+// console.log(validPalindrome('abca'));
+// console.log(validPalindrome('abbca'));
