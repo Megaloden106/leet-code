d07286e9f492093685de88c10db3e9e2550a4532
EddieLo <eddielo106@gmail.com>
2018-11-16 15:20:23 -0800

    init magic dictionary

--- README.md
+++ README.md
@@ -6,6 +6,7 @@
 ## List (Date - Problem / # - Difficulty)
  - 11/16
    - Binary Tree Level Order Traversal / 102 - Medium
+   - Implement Magic Dictionary / 676 - Medium
  - 11/15
    - Trapping Rain Water II / 407 - Hard
    - Remove Linked List Elements / 203 - Easy
---
+++ magic-dictionary.js
@@ -0,0 +1,39 @@
+// Implement a magic directory with buildDict, and search methods.
+
+// For the method buildDict, you'll be given a list of non-repetitive words to build a dictionary.
+
+// For the method search, you'll be given a word, and judge whether
+// if you modifyexactly one character into another character in this word,
+// the modified word is in the dictionary you just built.
+
+/**
+ * Initialize your data structure here.
+ */
+const MagicDictionary = function() {
+    
+};
+
+/**
+ * Build a dictionary through a list of words 
+ * @param {string[]} dict
+ * @return {void}
+ */
+MagicDictionary.prototype.buildDict = function(dict) {
+  
+};
+
+/**
+ * Returns if there is any word in the trie that equals to the given word after modifying exactly one character 
+ * @param {string} word
+ * @return {boolean}
+ */
+MagicDictionary.prototype.search = function(word) {
+  
+};
+
+/**
+ * Your MagicDictionary object will be instantiated and called as such:
+ * var obj = Object.create(MagicDictionary).createNew()
+ * obj.buildDict(dict)
+ * var param_2 = obj.search(word)
+ */
