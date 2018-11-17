ad92b4273d15325e7413616b53487b8091529806
EddieLo <eddielo106@gmail.com>
2018-11-16 16:13:06 -0800

    init super palindrome

---
+++ super-palindrome.js
@@ -0,0 +1,12 @@
+// Let's say a positive integer is a superpalindrome if it is a palindrome, and it is also the square of a palindrome.
+
+// Now, given two positive integers L and R (represented as strings), return the number of superpalindromes in the inclusive range [L, R].
+
+/**
+ * @param {string} L
+ * @param {string} R
+ * @return {number}
+ */
+const superpalindromesInRange = (L, R) => {
+    
+};
