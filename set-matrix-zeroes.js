f5e7c50d7ef7a7bbef74c121d8cdc8b8eec7927f
EddieLo <eddielo106@gmail.com>
2018-11-19 09:29:16 -0800

    complete set matrix zeroes

--- set-matrix-zeroes.js
+++ set-matrix-zeroes.js
@@ -3,7 +3,38 @@
 /**
  * @param {number[][]} matrix
  * @return {void} Do not return anything, modify matrix in-place instead.
+ * constant space
  */
 const setZeroes = (matrix) => {
-  
+  m = matrix.length;
+  n = matrix[0].length;
+  let rows = new Set();
+  let cols = new Set();
+  for (let i = 0; i < m; i += 1) {
+    for (let j = 0; j < n; j += 1) {
+      if (matrix[i][j] === 0) {
+        rows.add(i);
+        cols.add(j);
+      }
+    }
+  }
+  rows.forEach(e => {
+    for (let i = 0; i < n; i += 1) {
+      matrix[e][i] = 0;
+    }
+  })
+  cols.forEach(e => {
+    for (let i = 0; i < m; i += 1) {
+      matrix[i][e] = 0;
+    }
+  })
 };
+
+const i = [
+  [1,1,1],
+  [1,0,1],
+  [1,1,1]
+];
+
+setZeroes(i);
+console.log(i);
